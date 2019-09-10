8f4dc4232b2907942e808d5b198c34e2dccc8ae0
Bronson Quick <bronson@bronsonquick.com.au>
2019-09-10 17:50:29 +1000

    Fix dependencies

--- modules/percona_server/manifests/init.pp
+++ modules/percona_server/manifests/init.pp
@@ -16,13 +16,14 @@
 	 exec { 'dpkg_percona':
 		 command => 'sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb',
 		 path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-		 require => [ Exec['wget_percona_deb'], Exec['percona_apt_update'] ],
+		 require => Exec['wget_percona_deb'],
 		 onlyif  => "test -f percona-release_latest.$(lsb_release -sc)_all.deb",
 	 }
 
 	 exec { 'percona_apt_update':
 		 command => 'sudo apt-get update',
 		 path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+		 require => Exec['dpkg_percona'],
 	 }
 
 	 package { 'percona-server-server-5.7':
