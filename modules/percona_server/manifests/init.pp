2690813cb99fd3760d39a12ff46560e09495acd7
Bronson Quick <bronson@bronsonquick.com.au>
2019-09-10 17:45:53 +1000

    Make sure apt update runs

--- modules/percona_server/manifests/init.pp
+++ modules/percona_server/manifests/init.pp
@@ -3,7 +3,7 @@
  	$config
  ) {
 	 package { 'mysql-*':
-		 ensure => absent,
+		 ensure  => absent,
 		 require => Package['mysql-server']
 	 }
 
@@ -16,9 +16,13 @@
 	 exec { 'dpkg_percona':
 		 command => 'sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb',
 		 path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-		 require => Exec['wget_percona_deb'],
+		 require => [ Exec['wget_percona_deb'], Exec['percona_apt_update'] ],
 		 onlyif  => "test -f percona-release_latest.$(lsb_release -sc)_all.deb",
-		 notify  => Exec['apt_update']
+	 }
+
+	 exec { 'percona_apt_update':
+		 command => 'sudo apt-get update',
+		 path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 	 }
 
 	 package { 'percona-server-server-5.7':
